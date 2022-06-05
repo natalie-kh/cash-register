39d5a47f1841a4f0ad5f6971e8baada64a7b9e2e
Natalia Khasanzhinova <natalya.khasanzhinova@karuna.group>
2022-06-05 21:03:39 +0200

    RUBY: added offer model

---
+++ app/models/offer.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Сlass-reference that allows to create product_offer templates.
+
+# @params code [String] - offer code (must be unique)
+# @params description [String] - short description on logical idea of the offer
+# @params  required_keys [Array] - array of symbols, keys required for the offer.
+
+class Offer
+  include ModelHelper
+
+  TABLE = :offers
+  INDEX_KEY = :code
+
+  attr_reader :code, :description, :required_keys
+
+  def initialize(code, description, required_keys)
+    @code = code
+    @description = description
+    @required_keys = required_keys
+  end
+
+  def validate!
+    errors = OfferValidator.validate!(self)
+    raise ValidationError, errors unless errors.empty?
+  end
+end
